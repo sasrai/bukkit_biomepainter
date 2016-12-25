618295075d5cb4a1fc6cebda8d9634a848837a49
けいそ <sasrai@gmail.com>
2016-12-25 17:05:40 +0900

    バイオーム名入力時にBukkit名とMC名両方使えるようにした

--- src/main/java/jp/sasrai/biomepainter/BPCommandExecutor.java
+++ src/main/java/jp/sasrai/biomepainter/BPCommandExecutor.java
@@ -1,6 +1,8 @@
 package jp.sasrai.biomepainter;
 
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.block.Biome;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -32,13 +34,25 @@ public class BPCommandExecutor implements CommandExecutor {
 
         if (args.length == 0) { return onNoArgs((Player) sender); }
         else if (args[0].equalsIgnoreCase("set") && args.length >= 2) {
-            return onCommandSet((Player) sender, args[1]);
+            return onCommandSet((Player) sender, buildLongArgs(args));
         }
         else if (args[0].equalsIgnoreCase("give")) {
             return onCommandGiveTool((Player) sender);
         }
 
         return true;
+    }
+
+    private String buildLongArgs(String args[]) { return buildLongArgs(args, 1); }
+    private String buildLongArgs(String args[], int index) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = index; i < args.length; i++) {
+            if (i != index) sb.append(" ");
+            sb.append(args[i]);
+        }
+
+        return sb.toString();
     }
 
     private boolean onNoArgs(Player player) {
@@ -57,8 +71,15 @@ public class BPCommandExecutor implements CommandExecutor {
             plugin.getTool().setBiome(player, plugin.getBiomeList().getBiome(biomeId));
         } catch (NumberFormatException e) {
             try {
-                plugin.getTool().setBiome(player, plugin.getBiomeList().getBiome(biomeName));
+                if (plugin.getBiomeList().biomeExists(biomeName)) {
+                    Biome newBiome = plugin.getBiomeList().getBiome(biomeName);
+                    plugin.getTool().setBiome(player, newBiome);
+                    player.sendMessage("[BiomePainter] Set " + ChatColor.YELLOW + plugin.getBiomeList().getBiomeMCName(newBiome) + ChatColor.RESET + " biome. ");
+                } else {
+                    player.sendMessage("[BiomePainter] `" + ChatColor.YELLOW + biomeName + ChatColor.RESET + "` : Unknown biome.");
+                }
             } catch (Exception ex) {
+                player.sendMessage("[BiomePainter] Don't set biome.");
                 return false;
             }
         }
